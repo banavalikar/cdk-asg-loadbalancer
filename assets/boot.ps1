5f8a93f628dfb6e5164a343a85655ceec5e1270c
Banavalikar <Radhakrishna.Banavalikar@insight.com>
2020-10-26 08:17:16 +0000

    Updated Powershell message for sanity.

--- assets/boot.ps1
+++ assets/boot.ps1
@@ -3,7 +3,7 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 powershell.exe -Command Set-ExecutionPolicy RemoteSigned
 powershell.exe -Command Import-Module AWSPowerShell
 powershell.exe -Command New-Item -Path "c:\temp" -ItemType "directory" -Force
-Write-Host ("Adding Windows Defender exclude for CodeDeploy...")
+Write-Host ("Adding Windows Defender exclude for CodeDeploy agent...")
 powershell.exe -Command Add-MpPreference -ExclusionPath ("C:\temp")
 powershell.exe -Command Read-S3Object -BucketName code-deploy-agent-bucket -Key codedeploy-agent.msi -File c:\temp\codedeploy-agent.msi
 powershell.exe -Command c:\temp\codedeploy-agent.msi /quiet /l c:\temp\host-agent-install-log.txt
